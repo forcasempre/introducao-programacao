555b7bb699f0e700d1735653c12128fcf213316e
forcasempre <34011236+forcasempre@users.noreply.github.com>
2017-11-27 13:10:07 -0200

    Create Exerc01.c

---
+++ lista01/Exerc01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    // QUestão 1: Imprima a	mensagem: “É preciso fazer todos os	algoritmos para	aprender”.
+    
+    printf("“É preciso fazer todos os algoritmos para aprender”.\n");
+    system("pause");
+
+    return 0;
+}
+
