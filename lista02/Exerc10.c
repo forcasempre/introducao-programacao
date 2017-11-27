09a306c65e92f85bdec23cd5d88d96ab9ebce3e9
forcasempre <34011236+forcasempre@users.noreply.github.com>
2017-11-27 15:07:27 -0200

    Update Exerc10.c

--- lista02/Exerc10.c
+++ lista02/Exerc10.c
@@ -14,25 +14,28 @@ int main()
 Faça um programa que lê a quantidade de dias que o hóspede ficou no hotel e imprime a taxa a pagar.
 USANDO WHILE */
 
-//declaração de variáveis
-    float diaria1=50.00, txpg1;
-    int qtd1;
+//declaração das variáveis
+float diaria=50.00, txpg;
+int qtd;
 
 //entrada de dados
     printf("Digite a quantidade de dias de hospedagem: \n");
-    scanf("%d", &qtd1);
+    scanf("%d", &qtd);
     
-//condição para menor q 15 diárias    
-    if (qtd1<15) {
-        printf("O Valor da diária é R$ %.2f", qtd1*15.30);
-//condição para igual a 15 diárias         
-    } else if (qtd==15) {
-        printf("O Valor da diária é R$ %.2f", qtd1*10.00);
-//condição paramaior q 15 diárias         
-    } else {
-        printf("O Valor da diária é R$ %.2f", qtd1*8.50);
+//teste de cada condição com WHILE antes de imprimir    
+    while (qtd<15) {
+        printf("O valor da taxa a pagar é: %.2f", qtd*15.30);
+        break;
         }
-        system("pause");
+    while (qtd==15) {
+        printf("O valor da taxa a pagar é: %.2f", qtd*10.00);
+        break;
+        }
+    while (qtd>15) {
+        printf("O valor da taxa a pagar é: %.2f", qtd*8.50);
+        break;
+        }
+    system("pause");
 
     return 0;
 }
